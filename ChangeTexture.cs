a99fc74ee0ca6e8ffe2ca4d860d6ea7059b8c8d3
f-v-m <f_v_m@MacBook-Pro-fedorkovych.local>
2015-01-22 21:02:56 +0200

    hz

--- ChangeTexture.cs
+++ ChangeTexture.cs
@@ -6,6 +6,7 @@ public class ChangeTexture : MonoBehaviour{
 	public Texture TextureOver;
 	public Texture TextureInit;
 	public GameObject bullet;
+	public ParticleEmitter hz;
 
 	void OnMouseExit(){
 		renderer.material.mainTexture = TextureOver; 
@@ -13,14 +14,21 @@ public class ChangeTexture : MonoBehaviour{
 
 	void OnMouseOver(){ 
 		renderer.material.mainTexture = TextureInit; 
+		hz.emit=false;
+
 	}
 
 	void OnMouseUp(){
-		float speed = 20;
+/*		float speed = 20;
 		float step = speed * Time.deltaTime;
 		GameObject newbullet = (GameObject)Instantiate( bullet, new Vector3(2f,1.5f,-1f), Quaternion.identity);
 		//newbullet.transform.position = Vector3.MoveTowards (newbullet.transform.position, transform.position, step);
 		newbullet.transform.position = Vector3.Lerp (new Vector3(2f,1.5f,-1f), transform.position, 1);
+*/
+//		hz.emit=false;
+		//yield return new WaitForSeconds(2);
+		//hz.emit=false;
+
 	}
 
 } 
---
+++ RayCastShooting.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RayCastShooting : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
